14221bb954662d26abc0cdc60dca629ca8fc82ad
AkselA <akselahenriksen@gmail.com>
2018-04-23 20:21:59 +0200

    Alternative brownian noise, more advanced fade function

--- R/level.R
+++ R/level.R
@@ -1,4 +1,7 @@
 fitrange <- function(W, lower=-1, upper=1) {
+	if (length(W) == 0) {
+		W <- NA
+	}
 	if (lower > upper) warning("upper bound must be strictly larger than lower bound")
 	if (length(W) == 0) return(numeric(0))
 	newrange <- upper - lower
@@ -12,11 +15,17 @@ fitrange <- function(W, lower=-1, upper=1) {
 }
 
 fit01 <- function(W) {
+	if (length(W) == 0) {
+		W <- NA
+	}
 	oldrange <- max(W) - min(W)
 	(W - min(W)) / oldrange
 }
 
 fit11 <- function(W) {
+	if (length(W) == 0) {
+		W <- NA
+	}
 	oldrange <- max(W) - min(W)
 	(W - min(W)) * (2/oldrange) + -1
 }
@@ -73,7 +82,7 @@ comp.v <- function(x, thr=0.8, ratio=1/2) {
     x
 }
 
-fadein <- function(x, length=441) {
+fadeinl <- function(x, length=441) {
 	
 	lx <- length(x)
 	run <- 1:length
@@ -83,7 +92,7 @@ fadein <- function(x, length=441) {
 	x
 }
 
-fadeout <- function(x, length=441) {
+fadeoutl <- function(x, length=441) {
 	
 	lx <- length(x)
 	run <- (lx - length + 1):lx
@@ -93,3 +102,60 @@ fadeout <- function(x, length=441) {
 	x
 }
 
+fade <- function(x, len=441, shape=3, lg=100, len2=len, shape2=shape, lg2=lg) {
+	
+	if (len+len2 > length(x)) {
+		len <- len2 <- floor(length(x)/2)
+		warning("fade length longer than input vector")
+	}
+	
+	if (len2 == len & shape2 == shape & lg2 == lg) {
+		asym <- FALSE
+	} else {
+		asym <- TRUE
+	}
+
+	slope <- dbeta(seq(0, 1, length.out=len*2), shape, shape)
+	slope <- log(slope[1:len] + lg)
+	slope <- fit01(slope)	
+    slope <- slope[!is.na(slope)]
+
+    if (asym) {
+		slope2 <- dbeta(seq(0, 1, length.out=len2*2), shape2, shape2)
+		slope2 <- log(slope2[(len2+1):length(slope2)] + lg2)
+		slope2 <- fit01(slope2)
+        slope2 <- slope2[!is.na(slope2)]
+    } else {
+    	slope2 <- rev(slope)
+    }
+    
+    l1 <- length(x) - length(slope) - length(slope2)
+    ones <- rep(1, l1)
+    svec <- c(slope, ones, slope2)
+    
+    x * svec
+}
+
+fadein <- function(x, len=441, shape=3, lg=100) {
+	fade(x=x, len=len, shape=shape, len2=1)
+}
+
+fadeout <- function(x, len=441, shape=3, lg=100) {
+	fade(x=x, len2=len, shape2=shape, lg2=lg, len=1)
+}
+
+dcrem <- function(x, extra=TRUE, span=1) {
+	if (extra) {
+	    mod <- loess(x ~ seq_along(x), span=span)
+	    x <- medcentre(resid(mod))
+    }
+    
+    ft <- fft(x)
+    ft[c(1, length(ft))] <- 0
+    
+    if (extra) {
+        ft[c(2, length(ft) - 1)] <- 0.5 * ft[c(2, length(ft) - 1)]
+    }
+    
+    ifft(ft)/length(ft)
+}
--- R/noise.R
+++ R/noise.R
@@ -1,7 +1,7 @@
 
 
 
-noise <- function(l, type=c("uniform", "triangle", "binary", 
+noise <- function(l=10, type=c("uniform", "triangle", "binary", 
   "pink", "red", "brownian", "impulse", "pulse"), fs=44100) {
 	type <- match.arg(type)
 	outp <- switch(type, 
@@ -12,22 +12,24 @@ noise <- function(l, type=c("uniform", "triangle", "binary",
 	  	  fr <- 1/sqrt(seq(1, fs/2, length.out=l))
 	  	  ph <- runif(l, 10, 1000)
 	  	  re <- ifft(pol2sq(fr, ph))
-	  	  re/sqrt(l*fs/2)*2600
+	  	  re <- medcentre(re)*2600
+	  	  re/sqrt(l*fs/2)
 	  },
-	  red      =,
+	  red      = {
+	  	  fr <- 1/seq(1, fs/2, length.out=l)
+	  	  ph <- runif(l, 10, 1000)
+	  	  re <- ifft(pol2sq(fr, ph))
+	  	  re <- medcentre(re)*20000	
+	  	  re/sqrt(l*fs/2)  	  
+	  },
 	  brownian = {
 	  	  tr <- c((runif(l, -1, 1) * runif(l, -1, 1) * 0.05))
 	  	  cs <- cumsum(tr)
 	  	  md <- lm(cs ~ seq_along(cs))
-	  	  medcentre(resid(md))
+	  	  meancentre(resid(md)) * 10
 	  })
     outp <- as.vector(outp)
 	attr(outp, "samp.rate") <- fs
 	outp
 }
-
-set.seed(1)
-noise(80, "bin") %>>% 
-  (~ plot(., pch=16, cex=0.6)) %>>% 
-  lines(col="#00000066")
 
